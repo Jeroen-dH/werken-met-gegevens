4dad824cf69e7f81089a8244f63259dabfe8e985
Jeroen <99068895@Mydavici.nl>
2021-09-07 16:03:51 +0200

    bereleningen uitje

---
+++ feestlunch.py
@@ -0,0 +1,7 @@
+croisantje = 0.39
+print (croisantje * 17)
+stokbrood = 2.78
+print (stokbrood * 2)
+kortingsbon = -0.50
+print (kortingsbon * 3)
+print (croisantje * 17 + stokbrood * 2 + 3 * kortingsbon)
---
+++ speelhaldag.py
@@ -0,0 +1,4 @@
+kaartje = 7.50
+ritjeVIPVR = 0.37
+
+print (kaartje * 3 + ritjeVIPVR* 27)
