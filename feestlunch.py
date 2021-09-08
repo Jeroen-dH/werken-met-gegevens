293d6cf3ad7a9a6ad4048b40fafa02261ec098de
Jeroen <99068895@Mydavici.nl>
2021-09-08 10:07:13 +0200

    screenshot toegevoegd

--- feestlunch.py
+++ feestlunch.py
@@ -5,3 +5,4 @@ print (stokbrood * 2)
 kortingsbon = -0.50
 print (kortingsbon * 3)
 print (croisantje * 17 + stokbrood * 2 + 3 * kortingsbon)
+print ("De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
--- speelhaldag.py
+++ speelhaldag.py
@@ -2,3 +2,4 @@ kaartje = 7.50
 ritjeVIPVR = 0.37
 
 print (kaartje * 3 + ritjeVIPVR* 27)
+print ("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 32.49 euro")
